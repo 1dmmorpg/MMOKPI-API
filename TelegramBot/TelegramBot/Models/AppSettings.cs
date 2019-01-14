cb8804e0c3ad2a063a021cc3cbb24aac5dfbb4bc
Artem <36303538+KuriyamaMiraiUA@users.noreply.github.com>
2019-01-14 13:01:08 +0300

    Add files via upload

---
+++ TelegramBot/TelegramBot/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Models
+{
+    public class AppSettings
+    {
+        public static string Url { get; set; } = "https://URL:443/{0}";
+        public static string Name { get; set; } = "MMOKPI";
+        public static string Key { get; set; } = "687056656:AAHq_gtma0pgLCQmDMHcgNhvnC3ZgeZdxn4";
+    }
+}
