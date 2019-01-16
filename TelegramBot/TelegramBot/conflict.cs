875aa7048b1ee9eb7c467158067986203c8a51c2
Dima Matroskin <dima.matroskin062@gmail.com>
2019-01-16 13:09:54 +0200

    new features

--- TelegramBot/TelegramBot/conflict.cs
+++ TelegramBot/TelegramBot/conflict.cs
@@ -3,10 +3,12 @@ namespace TelegramBot
 {
     class Hello 
     {
-        static void Main() 
-        {
-            Console.WriteLine("Hello World!");
+        static int add(int a, int b) => (a+b);
 
+        public static void Main(string[] args){
+            int a = Console.ReadLine();
+            int b = Console.ReadLine();
+            Console.WriteLine(add(a, b));
             Console.ReadKey();
         }
     }
