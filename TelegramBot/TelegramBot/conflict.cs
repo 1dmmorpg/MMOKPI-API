94a66a8d2a90dc00d835957a6e6f792108125772
Dima Matroskin <dima.matroskin062@gmail.com>
2019-01-16 12:57:09 +0200

    new file added

---
+++ TelegramBot/TelegramBot/conflict.cs
@@ -0,0 +1,13 @@
+using System;
+namespace TelegramBot
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
